b7212651b79023b1541f255cfbc75c1cedc64688
Markus Mutas <markus@mutas.dev>
2025-07-28 13:33:04 +0200

    update images

--- FysikMedia/Media.lua
+++ FysikMedia/Media.lua
@@ -11,15 +11,14 @@ end
 
 -- Memes for Break-Timer
 FysikMedia.BreakMemes = {
-        -- [[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\ZarugarPeace.blp]], 256, 256},
-        -- {[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\ZarugarChad.blp]], 256, 147},
-        -- {[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\Overtime.blp]], 256, 256},
-        --{[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\TherzBayern.blp]], 256, 24},
-        -- {[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\senfisaur.blp]], 256, 256},
-        --{[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\schinky.blp]], 256, 256},
-        --{[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\TizaxHose.blp]], 202, 256},
-        --{[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\ponkyBanane.blp]], 256, 174},
-        --{[[Interface\AddOns\NorthernSkyRaidTools\Media\Memes\ponkyDespair.blp]], 256, 166},
         {[[Interface\AddOns\FysikMedia\Memes\dragane.png]], 256, 256},
-        {[[Interface\AddOns\FysikMedia\Memes\getCoined.png]], 195, 211},
+        {[[Interface\AddOns\FysikMedia\Memes\daitosus.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\draganepoop.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\guffyass.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kazmilbeard.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kazmilcat.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kazmilcope.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kazmilgirls.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kazmiltitanic.png]], 256, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\kylgequest.png]], 256, 256},
 }
