56de3506970404d483c3c699257b8f08ed1c13a1
Markus Mutas <markus@mutas.dev>
2025-07-29 15:03:02 +0200

    scale images

--- FysikMedia/Media.lua
+++ FysikMedia/Media.lua
@@ -19,7 +19,7 @@ FysikMedia.BreakMemes = {
         {[[Interface\AddOns\FysikMedia\Memes\draganetish.png]], 249, 263},
         {[[Interface\AddOns\FysikMedia\Memes\draganewtf.png]], 249, 263},
         {[[Interface\AddOns\FysikMedia\Memes\eggwa.png]], 184, 256},
-        {[[Interface\AddOns\FysikMedia\Memes\fknlatoss.png]], 144, 256},
+        {[[Interface\AddOns\FysikMedia\Memes\fknlatoss.png]], 256, 455},
         {[[Interface\AddOns\FysikMedia\Memes\guffyass.png]], 191, 256},
         {[[Interface\AddOns\FysikMedia\Memes\kazandsheb.png]], 256, 68},
         {[[Interface\AddOns\FysikMedia\Memes\kazmilbeard.png]], 358, 256},
@@ -28,6 +28,6 @@ FysikMedia.BreakMemes = {
         {[[Interface\AddOns\FysikMedia\Memes\kazmildrunk.png]], 191, 256},
         {[[Interface\AddOns\FysikMedia\Memes\kazmilgirls.png]], 250, 256},
         {[[Interface\AddOns\FysikMedia\Memes\kazmiltitanic.png]], 256, 190},
-        {[[Interface\AddOns\FysikMedia\Memes\kylgequest.png]], 256, 166},
+        {[[Interface\AddOns\FysikMedia\Memes\kylgequest.png]], 394.75, 256},
         {[[Interface\AddOns\FysikMedia\Memes\whoraclemicdown.png]], 256, 166},
     }
